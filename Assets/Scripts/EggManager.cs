310112e13df66b2ac397666791400f6829bb023b
Dane <dan.devlab@gmail.com>
2026-02-26 22:46:05 +0800

    fix add cloud

---
+++ Assets/Scripts/EggManager.cs
@@ -0,0 +1,49 @@
+using UnityEngine;
+using Unity.Netcode;
+
+public class EggManager : NetworkBehaviour
+{
+    [Header("Elements")]
+    [SerializeField] private GameObject eggPrefab;
+
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        GameManager.onGameStateChanged += GameStateChangedCallback;
+    }
+
+    public override void OnDestroy()
+    {
+        base.OnDestroy();
+        GameManager.onGameStateChanged -= GameStateChangedCallback;
+    }
+
+    private void GameStateChangedCallback(GameManager.State gameState)
+    {
+        switch (gameState)
+        {
+            case GameManager.State.Game:
+                SpawnEgg();
+                break;
+        }
+    }
+
+    private void SpawnEgg()
+    {
+        if (!IsServer) return;
+        if (eggPrefab == null)
+        {
+            Debug.LogError("EggManager: eggPrefab 未赋值！请在 Inspector 中把 Egg 预制体拖到 EggManager 的 Egg Prefab 字段。");
+            return;
+        }
+        var eggObj = Instantiate(eggPrefab, Vector2.up * 5, Quaternion.identity);
+        if (eggObj.TryGetComponent<NetworkObject>(out var netObj))
+            netObj.Spawn();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
