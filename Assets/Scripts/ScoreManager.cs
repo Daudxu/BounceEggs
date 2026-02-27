fcbfd9f1f9b58894f3ef93696e674f6a74367c16
xudan <dan.devlab@gmail.com>
2026-02-27 18:42:15 +0800

    fix update UpdateScoreText

--- Assets/Scripts/ScoreManager.cs
+++ Assets/Scripts/ScoreManager.cs
@@ -23,6 +23,7 @@ public class ScoreManager : NetworkBehaviour
     {
         if (!IsOwner) return;
         Egg.onFallInWater += EggFallInWaterCallback;
+        GameManager.onGameStateChanged += GameStateChangedCallback;
     }
     /// <summary>
     /// 销毁时取消所有事件订阅，避免内存泄漏和空引用
@@ -32,6 +33,25 @@ public class ScoreManager : NetworkBehaviour
         base.OnDestroy();
         try { NetworkManager.OnServerStarted -= NetworkManager_OnServerStarted; } catch { }
         try { Egg.onFallInWater -= EggFallInWaterCallback; } catch { }
+        try { GameManager.onGameStateChanged -= GameStateChangedCallback; } catch { }
+    }
+
+    private void GameStateChangedCallback(GameManager.State gameState)
+    {
+        switch (gameState)
+        {
+            case GameManager.State.Game:
+                ResetScores();
+                break;
+        }
+    }
+
+    private void ResetScores()
+    {
+        hostScore = 0;
+        clientScore = 0;
+        UpdateScoreClientRpc(hostScore, clientScore);
+        UpdateScoreText();
     }
 
     // Start is called once before the first execution of Update after the MonoBehaviour is created
@@ -56,10 +76,24 @@ public class ScoreManager : NetworkBehaviour
         {
             hostScore++;
         }
+        UpdateScoreClientRpc(hostScore, clientScore);
         UpdateScoreText();
     }
 
+    [ClientRpc]
+    private void UpdateScoreClientRpc(int hostScore, int clientScore)
+    {
+        this.hostScore = hostScore;
+        this.clientScore = clientScore;
+    }
+
     private void UpdateScoreText()
+    {
+        UpdateScoreTextClientRpc();
+    }
+
+    [ClientRpc]
+    private void UpdateScoreTextClientRpc()
     {
         scoreText.text = "<color=#00FF00>" + hostScore.ToString() + "</color> - <color=#0000FF>" + clientScore.ToString() + "</color>";
     }
