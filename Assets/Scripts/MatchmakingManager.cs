e2d6f777e71bfd124c33cd66b68f69b48e501754
xudan <dan.devlab@gmail.com>
2026-03-19 14:22:58 +0800

    fix update MatchmakingManager

---
+++ Assets/Scripts/MatchmakingManager.cs
@@ -0,0 +1,33 @@
+using UnityEngine;
+using Unity.Services.Core;
+using Unity.Services.Authentication;
+using System;
+
+public class MatchmakingManager : MonoBehaviour
+{
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    async void Authenticate()
+    {
+        try
+        {
+            await UnityServices.InitializeAsync();
+            await AuthenticationService.Instance.SignInAnonymouslyAsync();
+            var playerID = AuthenticationService.Instance.PlayerId;
+        }
+        catch (Exception e)
+        {
+            Debug.Log(e);
+        }
+    }
+}
